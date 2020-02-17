8d0bf87ed5801bba66bf12d91fea9742b4b52769
Cesar Alonso Llano Lagos <43044721+cesarAlonsolll@users.noreply.github.com>
2020-02-16 23:35:51 -0500

    iterator y coleccion

---
+++ IteradorOrdenes.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class IteradorOrdenes implements Iterator {
+
+    private final Map<Integer, Order> coleccion;
+    private int index = 0;
+    private final Integer[] keyArray;
+    private Order nextCandidate;
+
+    public IteradorOrdenes(Map<Integer, Order> coleccion) {
+        this.coleccion = coleccion;
+        keyArray = (Integer[]) coleccion.keySet().toArray();
+    }
+
+    public boolean hasNext() {
+        index++;
+        if(index < keyArray.length && keyArray[index] != null ){
+            nextCandidate = coleccion.get(keyArray[index]);
+            return true;
+        }
+        return false;
+    }
+
+    public Object next() {
+        if (nextCandidate == null) {
+            throw new NoSuchElementException();
+        } else {
+            return nextCandidate;
+        }
+    }
+
+}
